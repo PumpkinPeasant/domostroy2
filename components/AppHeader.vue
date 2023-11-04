85884e70e9de7bd2cdda1bc57f1537bb9daafb22
Anton <wanker@digitalglobe.by>
2023-11-04 21:37:22 +0300

    first section styling

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,34 +1,57 @@
 <template>
   <header>
-    <div>
-
+    <div class="header__logo">
+      <img :src="Logo">
+      <span>Домострой</span>
     </div>
-    <div>
-      <ul>
-        <li>Проекты</li>
-        <li>Ипотека</li>
-        <li>Виды работ</li>
-        <li>Контакты</li>
+    <div class="header__menu">
+      <ul role="list" class="header__menu-container">
+        <li class="header__menu-item">Проекты</li>
+        <li class="header__menu-item">Ипотека</li>
+        <li class="header__menu-item">Виды работ</li>
+        <li class="header__menu-item">Контакты</li>
       </ul>
     </div>
     <icon></icon>
     <icon></icon>
-    <button>Обратная связь</button>
+    <button class="button-primary">Обратная связь</button>
   </header>
 </template>
 
-<script>
-export default {
-  name: "AppHeader"
-}
+<script lang="ts" setup>
+import Logo from '~/assets/images/logo.svg'
+
 </script>
 
 <style scoped lang="scss">
 @import "assets/scss/_variables";
 
 header {
+  z-index: 1;
   position: fixed;
-  width: 100%;
   height: $header-width-desktop;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: -webkit-fill-available;
+
+  .header__logo {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    font-size: 20px;
+    font-weight: 700;
+  }
+
+  .header__menu {
+    &-container {
+      display: flex;
+      gap: 44px;
+      li{
+        cursor: pointer;
+      }
+    }
+  }
 }
+
 </style>
--- components/index/MainSection.vue
+++ components/index/MainSection.vue
@@ -20,7 +20,7 @@
         <span>Лет на рынке</span>
       </div>
     </div>
-<!--    <img :src="Roof">-->
+    <img class="roof-img" :src="Roof">
 
   </main>
 </template>
@@ -33,16 +33,31 @@ import Roof from '~/assets/images/roof.png'
 <style scoped lang="scss">
 @import "assets/scss/_variables";
 @import "assets/scss/_colors";
+
 main {
   padding-top: $header-width-desktop;
-  height: 100%;
+  height: calc(100vh - $header-width-desktop);
   background: radial-gradient(28.75% 30.78% at 102.5% 44.34%, rgba(45, 136, 204, 0.10) 0%, rgba(61, 116, 158, 0.00) 100%), radial-gradient(32.9% 49.24% at 46% 91.68%, rgba(65, 170, 248, 0.10) 0%, rgba(53, 153, 227, 0.00) 100%), radial-gradient(44.23% 42.9% at 0% 94.23%, rgba(45, 136, 204, 0.10) 0%, rgba(61, 116, 158, 0.00) 100%), radial-gradient(36.28% 47.06% at 46.03% 14.1%, rgba(45, 136, 204, 0.10) 0%, rgba(61, 116, 158, 0.00) 100%), #F8FEFE;
 }
-.numbers{
+
+h1{
+  margin-top: $header-width-desktop;
+}
+
+.numbers {
   display: flex;
-  .number{
+
+  .number {
     display: flex;
     flex-direction: column;
   }
+}
+
+.roof-img {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: auto;
+  height: 80%;
 }
 </style>
