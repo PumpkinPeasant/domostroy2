2dd1463c73a352090f1a9bb8414bd8ce631dc23a
dmitrij <bolshan_korp@mail.ru>
2024-04-03 13:19:01 +0300

    Создана основная страница сайта

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <MainSection id="top"/>
+  <BenefitsSection/>
+  <ProjectsSection id="projects"/>
+  <MortgageSection id="mortgage"/>
+  <ServicesSection id="services"/>
+  <ContactsSection id="contacts"/>
+</template>
+
+<script setup>
+
+import BenefitsSection from "~/components/index/BenefitsSection.vue";
+import MainSection from "~/components/index/MainSection.vue";
+import ServicesSection from "~/components/index/ServicesSection.vue";
+import ProjectsSection from "~/components/index/ProjectsSection.vue";
+import MortgageSection from "~/components/index/MortgageSection.vue";
+import ContactsSection from "~/components/index/ContactsSection.vue";
+</script>
+
+<style scoped>
+
+</style>
