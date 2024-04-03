387dc8faab9bb5363ce72a84367d1db5838fadaf
dmitrij <bolshan_korp@mail.ru>
2024-04-03 13:19:53 +0300

    Очистка app

--- app.vue
+++ app.vue
@@ -1,22 +1,8 @@
 <template>
   <NuxtLayout>
-    <MainSection/>
-    <BenefitsSection/>
-    <ProjectsSection/>
-    <MortgageSection/>
-    <ServicesSection/>
-    <ContactsSection/>
+    <NuxtPage/>
   </NuxtLayout>
 </template>
-<script>
-import MainSection from "./components/index/MainSection.vue";
-import BenefitsSection from "./components/index/BenefitsSection.vue";
-import ProjectsSection from "./components/index/ProjectsSection.vue";
-import MortgageSection from "./components/index/MortgageSection.vue";
-import ServicesSection from "./components/index/ServicesSection.vue";
-import ContactsSection from "./components/index/ContactsSection.vue";
 
-export default {
-  components: {ContactsSection, ServicesSection, MortgageSection, ProjectsSection, BenefitsSection, MainSection}
-}
+<script>
 </script>
