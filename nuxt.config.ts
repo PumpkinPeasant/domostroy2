7a16ee1b92ee829da645c24f2c41ffda3339f427
PumpkinPeasant <m2314670@edu.misis.ru>
2026-02-01 22:17:47 +0300

    Added lang to html

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,10 @@ export default defineNuxtConfig({
                     rel: 'stylesheet',
                     href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap'
                 },
-            ]
+            ],
+            htmlAttrs: {
+                lang: 'ru'
+            }
         },
     },
     build: {
