f1a7330ea7aee4f7247333eace335186d6070d31
PumpkinPeasant <m2314670@edu.misis.ru>
2026-02-01 23:56:06 +0300

    Header refactoring

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,284 +1,238 @@
 <template>
-  <header :class="navClass">
-    <div class="header__logo">
-      <NuxtLink to="/">
-        <nuxt-img height="40" width="40" src="/images/logo.svg" alt="Домострой логотип"/>
-        <span>Домострой</span>
-      </NuxtLink>
-    </div>
-    <nav v-if="!mobileSize" class="header__menu">
-      <ul class="header__menu-container">
-        <NuxtLink
-            v-for="link of links"
-            :key="link.to"
-            :to="link.to"
-            class="header__menu-item"
-            :class="{active: router.currentRoute.value.hash === link.to}"
-        >
-          {{ link.pageTitle }}
-        </NuxtLink>
-      </ul>
-    </nav>
-    <a v-if="!mobileSize" class="contact_section button-primary" href="tel:+7 (911) 563-43-75">
-      <nuxt-img height="20" width="20" src="/icons/phone-white.svg" alt="Иконка телефона"/>
-      +7 (911) 563-43-75
-    </a>
-    <div
-        v-if="mobileSize"
-        @click="toggleMenu"
-        class="menu"
-        style="cursor: pointer">
-      <nuxt-img height="16" width="23" src="/icons/menu.svg" alt="Иконка меню"/>
-    </div>
-  </header>
-  <Transition :duration="550" name="nested">
-    <div
-        v-if="openMenuFlag"
-        class="sidebar">
-      <div class="sidebar-header">
-        <div class="header__logo">
-          <NuxtLink to="/">
-            <nuxt-img height="40" width="40" src="/images/logo.svg" alt="Домострой логотип"/>
-            <span>Домострой</span>
-          </NuxtLink>
-        </div>
-        <div class="menu_icon" @click="toggleMenu">
-          <arrow/>
+  <div class="fade" :data-fade="toggleMenu"></div>
+  <header class="header" :class="[isScrolled && 'header-scrolled']">
+    <div class="container">
+      <div class="nav-wrapper">
+        <a class="nav-logo | d-flex align-center" href="#">
+          <NuxtImg
+              class="logo"
+              src="/images/logo.svg"
+              height="40"
+              alt="Логотип Домострой"
+              preload
+              fetchPriority="high"/>
+          <span>Домострой</span>
+        </a>
+        <nav aria-label="Primary"
+             :data-visible="toggleMenu"
+             class="primary-navigation"
+             id="primary-navigation">
+          <ul class="nav-list" role="list">
+            <li v-for="(item, index) in navItems" :key="index">
+              <nuxt-link :to="item.href" @click="toggleMenu = false">
+                {{ item.title }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </nav>
+        <div class="header-actions | d-flex align-center">
+          <a class="phone-number | d-flex align-center" style="--flex-gap: .5rem" href="tel:+79115634375">
+            <nuxt-img height="20" width="20" src="/icons/phone-white.svg" alt="Иконка телефона"/>
+            <span>+7 (911) 563-43-75</span>
+          </a>
+          <div class="nav-button | d-flex justify-center align-center" @click="toggle()" style="padding: var(--size-200)">
+            <NuxtImg class="user-select-none" :src="toggleMenu ? '/icons/chevron.svg' : '/icons/menu.svg'"
+                     alt="Меню"
+                     height="24" width="24"
+                     preload
+                     fetchPriority="high"/>
+          </div>
         </div>
       </div>
-      <NuxtLink
-          v-for="link of links"
-          @click="toggleMenu"
-          :key="link.to"
-          :to="link.to"
-          class="sidebar_menu"
-          :class="{active: router.currentRoute.value.hash === link.to}"
-      >
-        {{ link.pageTitle }}
-      </NuxtLink>
     </div>
-  </Transition>
-
+  </header>
 </template>
 
 <script setup lang="ts">
-import {ref, watch} from 'vue'
-import Arrow from "~/components/index/UI/Arrow.vue";
-
-const router = useRouter();
-const mobileSize = ref(true)
-const openMenuFlag = ref(false)
-const navClass = ref('')
-
-const links = ref([
+const navItems = [
   {
-    pageTitle: "Проекты",
-    to: "#projects",
+    title: "Проекты",
+    href: "#projects",
   },
   {
-    pageTitle: "Ипотека",
-    to: "#mortgage",
+    title: "Ипотека",
+    href: "#mortgage",
   },
   {
-    pageTitle: "Виды работ",
-    to: "#services",
+    title: "Виды работ",
+    href: "#services",
   },
   {
-    pageTitle: "Контакты",
-    to: "#contacts",
+    title: "Контакты",
+    href: "#contacts",
   }
-]);
+]
 
+const toggleMenu = ref(false)
+const isScrolled = ref(false)
+
+let toggle = () => {
+  toggleMenu.value = !toggleMenu.value
+}
 
 onMounted(() => {
   window.addEventListener("scroll", handleScroll);
-  window.scrollY > 0 ? navClass.value = "nav-scrolled" : ''
-  window.addEventListener("resize", handleWidth);
-  handleWidth()
 })
 
 onBeforeUnmount(() => {
   window.removeEventListener("scroll", handleScroll);
 })
 
-function handleScroll(event) {
-  if (event && window.scrollY > 0) {
-    navClass.value = "nav-scrolled";
-  } else {
-    navClass.value = "";
-  }
-}
-
-function toggleMenu() {
-  openMenuFlag.value = !openMenuFlag.value
-  const body = document.querySelector('body')
-  if (openMenuFlag.value) {
-    body.style.overflow = "hidden"
-  } else {
-    body.style.overflow = "auto"
-  }
-}
-
-function handleWidth() {
-  let width = window.innerWidth
-  mobileSize.value = width < 1000;
+function handleScroll() {
+  isScrolled.value = window.scrollY > 80
 }
 </script>
 
 <style scoped>
-header {
-  width: 100%;
-  background-color: transparent;
-  position: fixed;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  transition: 0.6s;
+.header {
   z-index: 3;
-  padding-top: 4rem;
-  padding-bottom: 4rem;
+  position: fixed;
+  width: 100%;
+
+  transition: background-color 0.6s ease;
 }
 
-.header__menu-container {
+.container {
+  --section-padding-block: 3rem;
+  transition: padding-block 0.6s ease;
+}
+
+.header-scrolled {
+  background: white;
+}
+
+.header-scrolled > .container {
+  --section-padding-block: 1.5rem;
+}
+
+.nav-wrapper {
   display: flex;
-  gap: 44px;
-}
-
-.header__menu-item {
-  cursor: pointer;
-  font-size: 16px;
-  color: var(--clr-neutral-500);
-  transition: font-weight 0.2s ease;
-}
-
-.header__menu-item:hover,
-.header__menu-item:active {
-  color: var(--clr-neutral-800);
-}
-
-header .active {
-  color: var(--clr-neutral-800);
-  font-weight: 600;
-}
-
-@media (max-width: 768px) {
-  header {
-    padding-top: 3rem;
-    padding-bottom: 3rem;
-  }
-}
-
-.header__logo a {
-  display: flex;
-  gap: 8px;
+  justify-content: space-between;
   align-items: center;
+}
+
+.nav-logo {
+  gap: 8px;
   font-size: 20px;
   font-weight: 700;
 }
 
-.toggleButton {
+.nav-list {
+  display: flex;
+  gap: clamp(var(--size-400), 5vw, var(--size-600));
+  font-size: var(--fs-nav);
+  color: var(--clr-neutral-500);
+}
+
+.nav-list li a:hover,
+.nav-list li a:focus {
+  color: var(--clr-neutral-900);
+}
+
+.phone-number {
+  background-color: var(--clr-neutral-800);
+  color: var(--clr-neutral-100);
+  padding-block: var(--size-200);
+  padding-inline: var(--size-600);
+}
+
+.phone-number:hover,
+.phone-number:focus {
+
+}
+
+.nav-button {
   display: none;
-}
-
-.nav-scrolled {
-  padding-top: 2rem;
-  padding-bottom: 2rem;
-  background-color: var(--clr-neutral-200);
-}
-
-@media (max-width: 768px) {
-  .nav-scrolled {
-    padding-top: 1.25rem;
-    padding-bottom: 1.25rem;
-  }
-}
-
-.contact_section {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 12px 30px;
-  font-size: 16px;
-}
-
-.contact_section img {
-  margin-right: 10px;
-}
-
-.menu {
-  transition: transform 0.2s ease;
-}
-
-.menu:active {
-  transform: translateY(2px);
-}
-
-.sidebar {
-  position: fixed;
-  z-index: 100000;
-  padding: 30px 25px;
-  display: flex;
-  height: 100vh;
-  flex-direction: column;
-  right: 0;
-  top: 0;
-  left: 0;
-  background: var(--clr-neutral-100);
-}
-
-.sidebar .sidebar-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.menu_icon {
   cursor: pointer;
 }
 
-.menu_icon svg {
-  color: var(--clr-neutral-800);
-  height: 20px;
-  transform: rotate(180deg);
+.header-actions {
+  --flex-gap: var(--size-200)
 }
 
-.sidebar_menu {
-  color: var(--clr-neutral-800);
-  font-size: 19px;
-  margin: 15px 0;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
+@media (max-width: 90em) {
+  .nav-list {
+    gap: clamp(var(--size-200), 1vw, var(--size-400));
+  }
+
+  .header-actions {
+    margin-left: var(--size-200);
+  }
 }
 
-.nested-enter-active,
-.nested-leave-active {
-  transition: all 0.3s ease-in-out;
-}
+@media (max-width: 50em) {
+  .container {
+    --section-padding-block: 1rem;
+  }
 
-.nested-leave-active {
-  transition-delay: 0.25s;
-}
+  .nav-logo{
+    z-index: 2;
+  }
 
-.nested-enter-from,
-.nested-leave-to {
-  transform: translateX(30px);
-  opacity: 0;
-}
+  .nav-logo span{
+    display: none;
+  }
 
-.nested-enter-active .inner,
-.nested-leave-active .inner {
-  transition: all 0.3s ease-in-out;
-}
+  .primary-navigation {
+    position: fixed;
+    top: 0; left: 0; right: 0;
+    padding-block: 85px var(--size-400);
+    background: var(--clr-neutral-100);
+    border-radius: var(--size-100);
+    box-shadow: 0 0 0.75em rgba(0, 0, 0, 0.05);
+    visibility: hidden;
+    opacity: 0;
+    transform: translateY(-10%);
+    transition: opacity 0.3s ease, transform 0.3s ease;
+  }
 
-.nested-enter-active .inner {
-  transition-delay: 0.25s;
-}
+  .primary-navigation[data-visible=true] {
+    visibility: visible;
+    opacity: 1;
+    transform: translateY(0);
+  }
 
-.nested-enter-from .inner,
-.nested-leave-to .inner {
-  transform: translateX(30px);
-  opacity: 0.001;
+  .fade {
+    position: fixed;
+    inset: 0;
+    z-index: -1;
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(10, 19, 35, 0.8));
+    opacity: 0;
+    transition: opacity 0.3s ease;
+  }
+
+  .fade[data-fade=true] {
+    opacity: 1;
+    z-index: 2;
+  }
+
+  .nav-list {
+    display: grid;
+    gap: 0;
+    text-align: center;
+    font-weight: var(--fw-bold);
+  }
+
+  .nav-list li a {
+    padding-block: var(--size-300);
+    display: block;
+    width: 100%;
+  }
+
+  .phone-number {
+    z-index: 2;
+
+    padding-inline: var(--size-400);
+  }
+
+  .nav-button {
+    z-index: 2;
+
+    display: unset;
+  }
+
+  .header-actions {
+    --flex-gap: 0;
+  }
 }
 </style>
