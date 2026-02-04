ce7ccfae0fc52974922147ffde88c246a25457ba
Anton <anton.n@prima-inform.ru>
2026-02-04 16:58:53 +0300

    Updated Ymaps api key

--- app/plugins/yandex.ts
+++ app/plugins/yandex.ts
@@ -1,9 +1,9 @@
-import { defineNuxtPlugin } from '#app'
+import {defineNuxtPlugin} from '#app'
 import Ymaps from 'vue-yandex-maps'
 
 
 const settings = {
-    apiKey: '004dbda2-6983-41ec-83be-7b74f8f7e68d', // Индивидуальный ключ API
+    apiKey: process.env.YANDEX_MAPS_API_KEY, // Индивидуальный ключ API
     lang: 'ru_RU', // Используемый язык
     // coordorder: 'latlong', // Порядок задания географических координат
     // debug: false, // Режим отладки
